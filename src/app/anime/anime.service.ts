24182f9df0118c00c647cc62661ffca29fbfbeb2
nlmz94 <nmz94140@gmail.com>
2021-03-25 15:27:21 +0100

    env var

--- src/app/anime/anime.service.ts
+++ src/app/anime/anime.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Anime} from '../shared/model/anime';
+import {environment} from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -11,10 +12,10 @@ export class AnimeService {
     constructor(private httpclient: HttpClient) {}
 
     getAllAnime(): Observable<Anime[]> {
-        return this.httpclient.get<Anime[]>('http://localhost:6671/animes');
+        return this.httpclient.get<Anime[]>(environment.backend + '/animes');
     }
 
     getOneAnime(id): Observable<Anime> {
-        return this.httpclient.get<Anime>('http://localhost:6671/animes/' + id);
+        return this.httpclient.get<Anime>(environment.backend + '/animes/' + id);
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    backend: 'http://api.onlyweebs.csid.agilitejoviale.fr'
 };
