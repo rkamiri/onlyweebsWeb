bfe60914438d38b3c2d73cbd0803ae64e2030259
nlmz94 <nmz94140@gmail.com>
2021-04-02 10:47:09 +0200

    # Conflicts: #	src/app/anime/anime.component.html #	src/app/anime/anime.component.ts

--- src/app/anime/anime.component.ts
+++ src/app/anime/anime.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {Anime} from '../shared/model/anime';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../shared/model/user';
 import {RatingService} from '../shared/service/rating.service';
 import {Rating} from '../shared/model/rating';
@@ -18,6 +18,7 @@ export class AnimeComponent implements OnDestroy, OnInit {
     public globalRate: number;
     rateForm: FormGroup;
     isUserAuthenticated: boolean;
+    userHasRated: string;
     navigationSubscription;
 
     constructor(private router: Router, private activatedRoute: ActivatedRoute, private ratingService: RatingService) {
@@ -29,6 +30,7 @@ export class AnimeComponent implements OnDestroy, OnInit {
         this.rateForm = new FormGroup({
             rate: new FormControl('')
         });
+        this.userHasRated = 'Add Personal Rate';
     }
 
     initialiseAnime() {
@@ -44,14 +46,17 @@ export class AnimeComponent implements OnDestroy, OnInit {
     ngOnInit(): void {
         this.anime = this.activatedRoute.snapshot.data.anime;
         this.currentUser = this.activatedRoute.snapshot.data.currentUser;
-        this.globalRate = this.activatedRoute.snapshot.data.globalRating.toFixed(1);
+        this.globalRate = this.activatedRoute.snapshot.data.globalRating;
         this.isUserAuthenticated = !!this.currentUser;
 
         if (this.isUserAuthenticated) {
-            if (!(this.currentRate == null)) {
+            if (this.currentRate === null || this.currentRate === undefined) {
                 this.currentRate = this.activatedRoute.snapshot.data.currentUserRating;
                 if (!(this.currentRate === 666)) {
                     this.rateForm.controls.rate.setValue(this.currentRate);
+                }
+                else{
+                    this.currentRate = undefined;
                 }
             }
         }
@@ -62,5 +67,22 @@ export class AnimeComponent implements OnDestroy, OnInit {
         console.log(rating);
         this.ratingService.putCurrentUserRatingOfAnAnime(rating).subscribe(
             () => { location.reload(); });
+    }
+
+    validateValue(event: number): void {
+        if (event > 10) {
+            if (event >= 100 && event < 200){
+                event = Number(event.toString().slice(0, 2));
+                console.log('here ' + event);
+
+            }
+            else{
+                event = Number(event.toString().slice(0, 1));
+            }
+            this.rateForm.setValue({rate: event});
+        }
+        if (event < 0) {
+            this.rateForm.setValue({rate: 0});
+        }
     }
 }
