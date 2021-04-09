624432e3165f2c6a8ab2774301a257e09b38bdeb
nlmz94 <nmz94140@gmail.com>
2021-04-09 14:27:52 +0200

    Merge of dev into master

--- src/app/list-create/list-create.component.spec.ts
+++ src/app/list-create/list-create.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateListComponent } from './create-list.component';
+import { ListCreateComponent } from './list-create.component';
 
-describe('CreateListComponent', () => {
-  let component: CreateListComponent;
-  let fixture: ComponentFixture<CreateListComponent>;
+describe('ListCreateComponent', () => {
+  let component: ListCreateComponent;
+  let fixture: ComponentFixture<ListCreateComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateListComponent ]
+      declarations: [ ListCreateComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CreateListComponent);
+    fixture = TestBed.createComponent(ListCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
