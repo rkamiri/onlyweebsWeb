3fec49260bf935eea37b7f99e4f201a67d0e0fce
rkamiri <rkamiri@iut.univ-paris8.fr>
2021-05-05 15:24:32 +0200

    added call to API still WIP

--- src/app/article-editor/article-editor.component.ts
+++ src/app/article-editor/article-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as DocumentEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Article} from '../shared/model/article';
+import {ArticleService} from '../shared/service/article.service';
 
 @Component({
   selector: 'app-article-editor',
@@ -8,10 +11,17 @@ import * as DocumentEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 })
 export class ArticleEditorComponent implements OnInit {
     public Editor = DocumentEditor;
-    public model: string;
-    constructor() { }
+    public body: string;
+    public articleInfos: FormGroup;
+    private formData: FormData;
+
+    constructor(private articleService: ArticleService ) { }
 
     ngOnInit(): void {
+        this.formData = new FormData();
+        this.articleInfos = new FormGroup({
+            title: new FormControl(''),
+        });
     }
     onReady( editor ): void {
         editor.ui.getEditableElement().parentElement.insertBefore(
@@ -20,7 +30,26 @@ export class ArticleEditorComponent implements OnInit {
         );
     }
 
-    onAziz(): void {
-        console.log(this.model);
+    fileChange(event): void  {
+        const fileList: FileList = event.target.files;
+        const file: File = fileList[0];
+        this.formData.append('uploadFile', file, file.name);
+    }
+    submit(): void {
+        console.log(this.formData.get('uploadFile'));
+        const article: Article = {
+            id: null,
+            title: this.articleInfos.get('title').value,
+            body: this.body,
+            created_at: null,
+            author: null,
+            cover: null,
+        };
+        let imageId;
+        this.articleService.postImage(this.formData).subscribe( data => {
+                imageId = data;
+            }
+        );
+        this.articleService.postArticle(article).subscribe();
     }
 }
