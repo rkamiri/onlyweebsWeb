273f6078963ebdc506395f10865a8eebf7baa7bc
nlmz94 <nmz94140@gmail.com>
2021-05-05 16:22:58 +0200

    Articles are properly displayed with all modifications, and all articles are listed

--- src/app/article-editor/article-editor.component.ts
+++ src/app/article-editor/article-editor.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as DocumentEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Article} from '../shared/model/article';
 import {ArticleService} from '../shared/service/article.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-article-editor',
-  templateUrl: './article-editor.component.html',
-  styleUrls: ['./article-editor.component.css']
+    selector: 'app-article-editor',
+    templateUrl: './article-editor.component.html',
+    styleUrls: ['./article-editor.component.css']
 })
 export class ArticleEditorComponent implements OnInit {
     public Editor = DocumentEditor;
@@ -15,7 +16,8 @@ export class ArticleEditorComponent implements OnInit {
     public articleInfos: FormGroup;
     private formData: FormData;
 
-    constructor(private articleService: ArticleService ) { }
+    constructor(private articleService: ArticleService, private route: Router) {
+    }
 
     ngOnInit(): void {
         this.formData = new FormData();
@@ -23,18 +25,20 @@ export class ArticleEditorComponent implements OnInit {
             title: new FormControl(''),
         });
     }
-    onReady( editor ): void {
+
+    onReady(editor): void {
         editor.ui.getEditableElement().parentElement.insertBefore(
             editor.ui.view.toolbar.element,
             editor.ui.getEditableElement()
         );
     }
 
-    fileChange(event): void  {
+    fileChange(event): void {
         const fileList: FileList = event.target.files;
         const file: File = fileList[0];
         this.formData.append('uploadFile', file, file.name);
     }
+
     submit(): void {
         console.log(this.formData.get('uploadFile'));
         const article: Article = {
@@ -45,11 +49,12 @@ export class ArticleEditorComponent implements OnInit {
             author: null,
             cover: null,
         };
-        let imageId;
-        this.articleService.postImage(this.formData).subscribe( data => {
-                imageId = data;
+        this.articleService.postImage(this.formData).subscribe(data => {
+                // setTimeout(() => {this.articleService.postArticle(article).subscribe();}, 500);
+                this.articleService.postArticle(article).subscribe((data) => {
+                    this.route.navigate(['/articles/' + data]).then();
+                });
             }
         );
-        this.articleService.postArticle(article).subscribe();
     }
 }
