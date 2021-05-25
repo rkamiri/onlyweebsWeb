4c83a1b57beb82a806059494e2779a8599ef6288
nlmz94 <nmz94140@gmail.com>
2021-05-25 16:45:49 +0200

    added a very small delay again

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,17 +19,23 @@ export class HomeComponent implements OnInit {
     public animes: Anime[];
     public imagesUrls: string[];
     articlesLoaded: boolean;
+    animesLoaded: boolean;
+    listsLoaded: boolean;
 
     constructor(private route: ActivatedRoute,
                 private articleService: ArticleService,
                 private animeService: AnimeService,
                 private listsService: ListsService) {
         this.articlesLoaded = false;
+        this.animesLoaded = false;
+        this.listsLoaded = false;
         this.imagesUrls = [];
     }
 
     ngOnInit(): void {
         this.fillArraysWithData();
+        this.setTimeOutForLists();
+        this.setTimeOutForAnimes();
         this.setTimeOutForArticles();
     }
 
@@ -42,6 +48,14 @@ export class HomeComponent implements OnInit {
             this.articles = data.slice(0, 5);
             data.forEach(article => this.imagesUrls.push(environment.backend + '/image/' + article.cover.id));
         });
+    }
+
+    setTimeOutForLists(): void {
+        setTimeout(() => this.listsLoaded = true, 50);
+    }
+
+    setTimeOutForAnimes(): void {
+        setTimeout(() => this.animesLoaded = true, 100);
     }
 
     setTimeOutForArticles(): void {
