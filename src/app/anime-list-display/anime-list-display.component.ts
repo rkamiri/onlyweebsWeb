30dfe090a475f7ff5bfab9d682133f9d5d65951d
Decnox <loick.ca@hotmail.fr>
2021-06-02 23:06:50 +0200

    merging with dev

---
+++ src/app/anime-list-display/anime-list-display.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Lists } from '../shared/model/lists';
+import { ActivatedRoute } from '@angular/router';
+import { ListsService } from '../shared/service/lists.service';
+
+@Component({
+    selector: 'app-anime-list-display',
+    templateUrl: './anime-list-display.component.html',
+    styleUrls: ['./anime-list-display.component.css'],
+})
+export class AnimeListDisplayComponent implements OnInit {
+    @Input()
+    lists: Lists[];
+    @Input()
+    isDefault: string;
+    listsImages: [[]];
+
+    constructor(
+        private route: ActivatedRoute,
+        private listService: ListsService
+    ) {}
+
+    ngOnInit(): void {
+        if (this.isDefault === 'user_default') {
+            this.listService
+                .getImagesOfUserDefaultList()
+                .subscribe((data) => (this.listsImages = data));
+        } else if (this.isDefault === 'user_custom') {
+            this.listService
+                .getImagesOfUserCustomList()
+                .subscribe((data) => (this.listsImages = data));
+        } else {
+            this.listService
+                .getImagesOfCustomLists()
+                .subscribe((data) => (this.listsImages = data));
+        }
+    }
+
+    delete(id: number): void {
+        if (confirm('Are you sure you want to delete this custom list ?')) {
+            this.listService.deleteList(id).subscribe(() => location.reload());
+        }
+    }
+}
