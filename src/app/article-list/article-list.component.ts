e892e0398ce1627fa9ff8493c5ec589047735815
rkamiri <rkamiri@iut.univ-paris8.fr>
2021-06-11 09:35:09 +0200

    added load more to article

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../shared/service/article.service';
 import { Article } from '../shared/model/article';
 import { environment } from '../../environments/environment';
+import { ArticleCategories } from '../shared/model/articleCategories';
 
 @Component({
     selector: 'app-article-list',
@@ -12,13 +13,15 @@ export class ArticleListComponent implements OnInit {
     public listArticles: Article[];
     public listArticlesRunner: Article[];
     public articleImageUrls: string[];
-
+    public categories: ArticleCategories[];
+    public selectedCategory: ArticleCategories;
+    public page = 0;
     constructor(private articleService: ArticleService) {
         this.articleImageUrls = [];
     }
 
     ngOnInit(): void {
-        this.articleService.getAllArticles().subscribe((data) => {
+        this.articleService.getArticlesByPage(this.page).subscribe((data) => {
             this.listArticles = data;
             data.forEach((article) =>
                 this.articleImageUrls.push(
@@ -32,5 +35,23 @@ export class ArticleListComponent implements OnInit {
                 );
             }
         });
+        this.articleService.getAllCategories().subscribe((data) => {
+            this.categories = data;
+        });
+    }
+    loadMore(): void {
+        this.page++;
+        this.articleService.getArticlesByPage(this.page).subscribe((data) => {
+            data.forEach((article) =>
+                this.articleImageUrls.push(
+                    environment.backend + '/image/' + article.cover.id
+                )
+            );
+            this.listArticlesRunner = this.listArticlesRunner.concat(data);
+            console.log(this.listArticlesRunner);
+        });
+    }
+    changeCategory($event: ArticleCategories): void {
+        this.selectedCategory = $event;
     }
 }
