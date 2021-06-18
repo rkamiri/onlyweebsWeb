fade2dd98352d298b28b1d66be22db6f3a388245
nlmz94 <nmz94140@gmail.com>
2021-06-18 10:46:48 +0200

    hotfixed a button

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Lists } from '../shared/model/lists';
 import { ActivatedRoute } from '@angular/router';
 import { ListsService } from '../shared/service/lists.service';
+import { UserService } from '../shared/service/user.service';
+import { User } from '../shared/model/user';
 
 @Component({
     selector: 'app-lists',
@@ -11,13 +13,16 @@ import { ListsService } from '../shared/service/lists.service';
 export class ListsComponent implements OnInit {
     public lists: Lists[];
     public listsImages: [[]];
+    public currentUser: User;
 
     constructor(
         private route: ActivatedRoute,
-        private listService: ListsService
+        private listService: ListsService,
+        private userService: UserService
     ) {}
 
     ngOnInit(): void {
+        this.userService.getCurrentUser().subscribe((data) => this.currentUser = data);
         this.lists = this.route.snapshot.data.customLists;
         this.listService
             .getImagesOfCustomLists()
