cad1b2fb9aefea029465061e436efff4af0ebd7e
nlmz94 <nmz94140@gmail.com>
2021-06-26 16:40:01 +0200

    Added new files, sma as the commit before.

---
+++ src/app/shared/resolver/stats/average.stats.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    Resolve,
+    RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AverageStats } from '../../model/average.stats';
+import { StatsService } from '../../service/stats.service';
+
+@Injectable({ providedIn: 'root' })
+export class AverageStatsResolver implements Resolve<AverageStats> {
+    constructor(private service: StatsService) {}
+
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<AverageStats> {
+        return this.service.getCommentStats();
+    }
+}
